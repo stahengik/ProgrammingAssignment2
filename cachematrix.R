724d89ae2b2534272e9ad24e39e98dd369292cbf
Singhal <181682@cognizant.com>
2018-11-18 21:46:17 -0500

    Programming Assignment 2, Code for Cahche Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x<<-y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inv <<- solve
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse= getinverse)
 
 }
 
@@ -12,4 +21,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setinverse(inv)
+  inv
 }
+
+
+set.seed(1123)
+mat4inv <- matrix(rnorm(25), nrow = 5, ncol = 5)
+
+cacheMat <- makeCacheMatrix(mat4inv)
+cacheSolve(cacheMat)
+solve(mat4inv)
